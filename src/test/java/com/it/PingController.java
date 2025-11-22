362a5504b9e69d6e53e52a89cb6fee9858f63656
Maxime Van den Kerkhof <maximevdk@me.com>
2025-11-22 21:27:37 +0100

    Path param

--- src/test/java/com/it/PingController.java
+++ src/test/java/com/it/PingController.java
@@ -1,9 +1,6 @@
 package com.it;
 
-import com.yawl.annotations.GetMapping;
-import com.yawl.annotations.PostMapping;
-import com.yawl.annotations.QueryParam;
-import com.yawl.annotations.WebController;
+import com.yawl.annotations.*;
 import com.yawl.model.HttpStatus;
 
 @WebController(path = "ping")
@@ -15,14 +12,13 @@ public class PingController {
         this.pingService = pingService;
     }
 
-    @GetMapping
-    public Pong ping(@QueryParam(name = "name") String name) {
-        return pingService.get(name);
+    @GetMapping(path = "/{id}")
+    public Pong ping(@PathParam(name = "name") String id) {
+        return pingService.get(id);
     }
 
     @PostMapping(status = HttpStatus.ACCEPTED)
     public Pong setPing(@QueryParam(name = "name") String name) {
-        pingService.set(name);
-        return new Pong("POST " + name);
+        return pingService.set(name);
     }
 }
